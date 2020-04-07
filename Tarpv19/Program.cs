bb109c80a88910d890bfbca4034b5a215452b97d
ADMIN <daniel.mihol@gmail.com>
2020-04-07 12:06:10 +0300

    Create Hello App

---
+++ Tarpv19/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarpv19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow ward");
+
+            Console.ReadLine();
+        }
+    }
+}
